155a161c56688508463c5daf2e19f13c5988e08b
Vix Hawley <victoria.hawley@spikeglobal.com>
2023-10-28 22:23:40 +0100

    practice embed flipbook

--- src/App.js
+++ src/App.js
@@ -3,21 +3,12 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{background: 'black', height: '100vh'}}>
+      
+      <div style={{padding: '10%', background: 'black'}}>
+      <h1 style={{color: 'white'}}>Example Flipbook</h1>
+      <iframe src="https://publuu.com/flip-book/282983/660819/page/1?embed" width="100%" height="580" scrolling="no" frameborder="0" allowfullscreen="" allow="clipboard-write" class="publuuflip"></iframe>
+      </div>
     </div>
   );
 }
